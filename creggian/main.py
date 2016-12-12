7b600963b30baf9108996d2e4eb5b3a4b9641135
Claudio Reggiani <nophiq@gmail.com>
2016-12-12 08:13:04 +0100

    updates

--- creggian/main.py
+++ creggian/main.py
@@ -44,8 +44,8 @@ def split_tsv(x):
 
 
 def parse_narrow_peak(x):
-    return (x[0], int(x[1]), int(x[2]), x[3], int(x[4]), x[5], float(x[6]), float(x[7]), float(x[8]), int(x[9])) + (x[10:],)
+    return (x[0], int(x[1]), int(x[2]), x[3], int(x[4]), x[5], float(x[6]), float(x[7]), float(x[8]), int(x[9])) + tuple(x[10:],)
 
 
 def parse_tfbs_ucsc(x):
-    return (x[0], int(x[1]), int(x[2]), x[3], int(x[4])) + (x[5:],)
+    return (x[0], int(x[1]), int(x[2]), x[3], int(x[4])) + tuple(x[5:],)
