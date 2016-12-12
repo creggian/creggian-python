dff95edc456f1bcf07d83c9ad921c2931cd2fa8f
Claudio Reggiani <nophiq@gmail.com>
2016-12-12 08:09:11 +0100

    updates

--- creggian/main.py
+++ creggian/main.py
@@ -41,3 +41,11 @@ def to_tsv_line(data):
 
 def split_tsv(x):
     return x.split("\t")
+
+
+def parse_narrow_peak(x):
+    return (x[0], int(x[1]), int(x[2]), x[3], int(x[4]), x[5], float(x[6]), float(x[7]), float(x[8]), int(x[9])) + (x[10:],)
+
+
+def parse_tfbs_ucsc(x):
+    return (x[0], int(x[1]), int(x[2]), x[3], int(x[4])) + (x[5:],)
